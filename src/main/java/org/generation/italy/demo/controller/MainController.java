46079346e6ab040f57d2fb030e5090538fbb7909
Antonio Mendetta <Antonio@MBP-di-Antonio.station>
2022-12-02 14:09:45 +0100

    print movies ArrayList in best-movie.html

--- src/main/java/org/generation/italy/demo/controller/MainController.java
+++ src/main/java/org/generation/italy/demo/controller/MainController.java
@@ -1,6 +1,11 @@
 package org.generation.italy.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.generation.italy.demo.pojo.Movie;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,15 +17,27 @@ public class MainController {
 		return "home";
 	}
 	
+	
+	@RequestMapping("/best-movie")
+	public String getBestMovie(Model model) {
+		
+		List<Movie> movies = new ArrayList<>();
+		
+		movies.add(new Movie("Primo"));
+		movies.add(new Movie("Secondo"));
+		movies.add(new Movie("Terzo"));
+		movies.add(new Movie("Quarto"));
+		movies.add(new Movie("Quinto"));
+		
+		model.addAttribute("movies", movies);
+		
+		return "best-movie";
+	}
+	
+
 	@RequestMapping("/best-song")
 	public String getBestSong() {
 		
 		return "best-song";
-	}
-	
-	@RequestMapping("/best-movie")
-	public String getBestMovie() {
-		
-		return "best-movie";
 	}
 }
---
+++ src/main/java/org/generation/italy/demo/pojo/Movie.java
@@ -0,0 +1,19 @@
+package org.generation.italy.demo.pojo;
+
+public class Movie {
+
+	private String name;
+	
+	public Movie(String name) {
+		
+		setName(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
